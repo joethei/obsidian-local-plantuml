3b206e3f982d408e327c427ae489ba90b414664c
Johannes Theiner <kontakt@joethei.xyz>
2021-12-12 11:17:48 +0100

    first release

--- main.ts
+++ main.ts
@@ -1,137 +1,96 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {Plugin} from 'obsidian';
+import {resolve} from 'path';
+import {exec} from "child_process";
 
-// Remember to rename these classes and interfaces!
+export default class LocalPlantUmlPlugin extends Plugin {
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+	//adapted from: https://github.com/agirorn/plantuml/blob/master/lib/plantuml.js & https://github.com/marcusolsson/obsidian-vale/blob/main/src/vale/ValeCli.ts
+	async generateImage(uml: string, path: string, output: string) : Promise<string> {
+		const jar = resolve(__dirname, path);
+		const args = [
+			'-jar',
+			'-Djava.awt.headless=true',
+			'--add-opens=java.xml/com.sun.org.apache.xalan.internal.xsltc.trax="ALL-UNNAMED"',
+			jar,
+			'-t' + output,
+			'-pipe'
+		];
+		const child = exec('java ' + args.join(" "), {encoding: 'binary'});
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+		let stdout: any;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+		if (child.stdout) {
+			child.stdout.on("data", (data) => {
+				if(stdout === undefined) {
+					stdout = data;
+				}else stdout += data;
+			});
+		}
+
+		return new Promise((resolve, reject) => {
+			child.on("error", reject);
+
+			child.on("close", (code) => {
+				if (code === 0) {
+					if(output === "png") {
+						const buf = new Buffer(stdout, 'binary');
+						resolve(buf.toString('base64'));
+					}
+					resolve(stdout);
+				} else if (code === 1) {
+					reject(new Error(`an error occurred`));
+				} else {
+					reject(new Error(`child exited with code ${code}`));
+				}
+			});
+
+			child.stdin.write(uml);
+			child.stdin.end();
+		});
+	}
+
+	async generateMap(uml: string, path: string) : Promise<string> {
+		const jar = resolve(__dirname, path);
+		const args = [
+			'-jar',
+			'-Djava.awt.headless=true',
+			'--add-opens=java.xml/com.sun.org.apache.xalan.internal.xsltc.trax="ALL-UNNAMED"',
+			jar,
+			'-pipemap'
+		];
+		const child = exec('java ' + args.join(" "), {encoding: 'binary'});
+
+		let stdout = "";
+
+		if (child.stdout) {
+			child.stdout.on("data", (data) => {
+				stdout += data;
+			});
+		}
+
+		return new Promise((resolve, reject) => {
+			child.on("error", reject);
+
+			child.on("close", (code) => {
+				if (code === 0) {
+					resolve(stdout);
+				} else if (code === 1) {
+					reject(new Error(`an error occurred`));
+				} else {
+					reject(new Error(`child exited with code ${code}`));
+				}
+			});
+
+			child.stdin.write(uml);
+			child.stdin.end();
+		});
+	}
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		console.log("loading plugin local PlantUML");
 	}
 
 	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		console.log("unloading plugin local PlantUML");
 	}
 }
